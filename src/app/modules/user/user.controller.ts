c3b28b668ffc35d581b53509eff0de98ea4e2f20
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-08-04 21:42:31 +0600

    refactor: standardize code formatting and improve consistency in user controller and service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,43 +1,43 @@
-import { RequestHandler } from 'express'
-import catchAsync from '../../utiles/catchAsync'
-import { UserService } from './user.service'
-import sendResponse from '../../utiles/sendResponse'
-import httpStatus from 'http-status'
+import { RequestHandler } from "express";
+import catchAsync from "../../utiles/catchAsync";
+import { UserService } from "./user.service";
+import sendResponse from "../../utiles/sendResponse";
+import httpStatus from "http-status";
 
 const createUser: RequestHandler = catchAsync(async (req, res) => {
-  const result = await UserService.createUserInDB(req.body)
+  const result = await UserService.createUserInDB(req.body);
 
   sendResponse(res, {
     data: result,
     success: true,
     statusCode: httpStatus.CREATED,
-    message: 'User registered successfully',
-  })
-})
+    message: "User registered successfully",
+  });
+});
 const getAllUsers: RequestHandler = catchAsync(async (req, res) => {
-  const result = await UserService.getAllUserFromDB(req.query)
+  const result = await UserService.getAllUserFromDB();
   sendResponse(res, {
     success: true,
     data: result,
-    message: 'Successfully get all Users from database',
+    message: "Successfully get all Users from database",
     statusCode: httpStatus.OK,
-  })
-})
+  });
+});
 
 const getMe = catchAsync(async (req, res) => {
-  const email = req.user?.email
-  const result = await UserService.getMe(email)
+  const email = req.user?.email;
+  const result = await UserService.getMe(email);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User is retrieved succesfully',
+    message: "User is retrieved succesfully",
     data: result,
-  })
-})
+  });
+});
 
 export const UserController = {
   createUser,
   getAllUsers,
   getMe,
-}
+};
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,27 +1,22 @@
-import QueryBuilder from '../../builder/QueryBuilder'
-import { TUser } from './user.interface'
-import { User } from './user.model'
+import { TUser } from "./user.interface";
+import { User } from "./user.model";
 
 const createUserInDB = async (payload: TUser) => {
-  const result = await User.create(payload)
-  return result
-}
+  const result = await User.create(payload);
+  return result;
+};
 
-const getAllUserFromDB = async (query: Record<string, unknown>) => {
-  const searchableFields = ['title', 'author']
-  const UserQuery = new QueryBuilder(User.find(), query)
-    .search(searchableFields)
-    .filter()
-  const result = await UserQuery.modelQuery
-  return result
-}
+const getAllUserFromDB = async () => {
+  const result = await User.find();
+  return result;
+};
 const getMe = async (email: string) => {
-  const result = await User.findOne({ email: email })
-  return result
-}
+  const result = await User.findOne({ email: email });
+  return result;
+};
 
 export const UserService = {
   createUserInDB,
   getMe,
   getAllUserFromDB,
-}
+};
