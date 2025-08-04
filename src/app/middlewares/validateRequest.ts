5ef1720545e39a5338f8a39d42c64bfacc65ddce
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-08-04 21:30:20 +0600

    refactor: update validateRequest to use ZodObject for schema validation

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,13 +1,13 @@
-import { AnyZodObject } from 'zod'
-import catchAsync from '../utiles/catchAsync'
-import { NextFunction, Request, Response } from 'express'
+import { ZodObject } from "zod";
+import catchAsync from "../utiles/catchAsync";
+import { NextFunction, Request, Response } from "express";
 
-const validateRequest = (schema: AnyZodObject) => {
+const validateRequest = (schema: ZodObject) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     await schema.parseAsync({
       body: req.body,
-    })
-    next()
-  })
-}
-export default validateRequest
+    });
+    next();
+  });
+};
+export default validateRequest;
